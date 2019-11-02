20991d9b8d46f7bcd1fc4cc51f0993b366fcf8e8
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-11-02 13:43:21 +1300

    React working

--- contact.js
+++ contact.js
@@ -1,82 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Contact from './contact.html';
-
-
-// const domContainer = document.querySelector('#contact.html');
-// ReactDOM.render(e(LikeButton), domContainer);
-
-// class MyForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       username: '',
-//       age: null,
-//       errormessage: ''
-//     };
-//   }
-//   myChangeHandler = (event) => {
-//     let nam = event.target.name;
-//     let val = event.target.value;
-//     let err = '';
-//     if (nam === "age") {
-//       if (val !="" && !Number(val)) {
-//         err = <strong>Your age must be a number</strong>;
-//       }
-//     }
-//     this.setState({errormessage: err});
-//     this.setState({[nam]: val});
-//   }
-//   render() {
-//     return (
-//       <form>
-//       <h1>Hello {this.state.username} {this.state.age}</h1>
-//       <p>Enter your name:</p>
-//       <input
-//         type='text'
-//         name='username'
-//         onChange={this.myChangeHandler}
-//       />
-//       <p>Enter your age:</p>
-//       <input
-//         type='text'
-//         name='age'
-//         onChange={this.myChangeHandler}
-//       />
-//       {this.state.errormessage}
-//       </form>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<MyForm />, document.getElementById('root'));
-
-
 'use strict';
 
 const e = React.createElement;
 
 class MyForm extends React.Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {
-      username: '',
-      age: null,
-      email: '',
-      message: '',
-      errormessage: ''
-    };
+    this.state = { liked: false };
   }
 
-  render () {
-    return (
-      <form>
-        <h1>test</h1>
-      </form>
-    );
-  }
-
-/*
   render() {
     if (this.state.liked) {
       return 'You liked this.';
@@ -88,8 +19,7 @@ class MyForm extends React.Component {
       'Like'
     );
   }
-  */
 }
 
-const domContainer = document.querySelector('#contactform');
+const domContainer = document.querySelector('#contact_container');
 ReactDOM.render(e(MyForm), domContainer);
