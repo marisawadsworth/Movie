cfc281d35d8beab8d70c3f8bcfa014c0d2c85b5a
Marisa Wadsworth <marisawadsworth@Marisas-MacBook-Air.local>
2019-11-04 14:46:22 +1300

    React working started form

--- src/contact.js
+++ src/contact.js
@@ -5,18 +5,45 @@ const e = React.createElement;
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { 
+      username: '',
+      age: null,
+      message: '',
+      errormessage: ''
+    };
+  }
+
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    let err = '';
+    if (nam === "age") {
+      if (val !="" && !Number(val)) {
+        err = <strong>Your age must be a number</strong>;
+      }
+    }
+    this.setState({errormessage: err});
+    this.setState({[nam]: val});
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    return (
+      <form>
+      <h1>Hello {this.state.username} {this.state.age}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your age:</p>
+      <input
+        type='text'
+        name='age'
+        onChange={this.myChangeHandler}
+      />
+      {this.state.errormessage}
+      </form>
     );
   }
 }
