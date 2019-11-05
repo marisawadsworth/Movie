08e0e00c56709757c186b8462351c3311d375e95
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-11-05 13:49:50 +1300

    React Form Done

--- src/contact.js
+++ src/contact.js
@@ -6,43 +6,53 @@ class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      username: '',
-      age: null,
-      message: '',
-      errormessage: ''
-    };
-  }
-
-  myChangeHandler = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value;
-    let err = '';
-    if (nam === "age") {
-      if (val !="" && !Number(val)) {
-        err = <strong>Your age must be a number</strong>;
+      fullname: '',
+      email: '',
+      message: ''
+      // errormessage: ''
       }
-    }
-    this.setState({errormessage: err});
-    this.setState({[nam]: val});
-  }
+    };
+  
+
+  // myChangeHandler = (event) => {
+  //   let nam = event.target.name;
+  //   let val = event.target.value;
+  //   let err = '';
+  //   if (nam === "age") {
+  //     if (val !="" && !Number(val)) {
+  //       err = <strong>Your age must be a number</strong>;
+  //     }
+  //   }
+  //   this.setState({errormessage: err});
+  //   this.setState({[nam]: val});
+  // }
 
   render() {
     return (
       <form>
-      <h1>Hello {this.state.username} {this.state.age}</h1>
-      <p>Enter your name:</p>
-      <input
-        type='text'
-        name='username'
-        onChange={this.myChangeHandler}
-      />
-      <p>Enter your age:</p>
-      <input
-        type='text'
-        name='age'
-        onChange={this.myChangeHandler}
-      />
-      {this.state.errormessage}
+        <h1>Contact Form</h1>
+          <input class="contact-input"
+            placeholder='Name'
+            type='text'
+            name='fullname'
+            onChange={this.myChangeHandler}
+          />
+        <p></p>
+          <input class="contact-input"
+            placeholder='Email'
+            type='text'
+            name='email'
+            onChange={this.myChangeHandler}
+          />
+        <p></p>
+          <input class="contact-input-message"
+            placeholder='Message'
+            type='text'
+            name='message'
+            onChange={this.myChangeHandler}
+          />
+        {/* {this.state.errormessage} */}
+        <button class="contact-input-button">Submit</button>
       </form>
     );
   }
